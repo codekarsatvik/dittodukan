9542b5650e8e96e7ce4c4e10b9c8add007396e3f
satvik aggarwal <skaggarwal2001.com@gmail.com>
2021-09-25 21:04:00 +0530

    cdn added

--- baniyanagri/settings.py
+++ baniyanagri/settings.py
@@ -15,7 +15,7 @@ from pathlib import Path
 import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 from django.conf import settings
-# from django.conf.urls.static import static
+from django.conf.urls.static import static
 
 # BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -126,10 +126,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-# STATICFILES_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL='/media/'
 MEDIA_ROOT=BASE_DIR / 'media'
 LOGIN_REDIRECT_URL='/checklogin/'
